a3f991992dafdba0fd438b5a53f97e813c026981
Luiz Fernando Oliveira <lf.nandooliveeira@gmail.com>
2025-06-25 18:02:17 -0300

    feat: complete project, add feature load and concat xls data

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
 from pipeline.extract import extract_data_from_excel
+from pipeline.transform import concat_dataframe
+from pipeline.load import load_excel
 
-lista_de_data_frame = extract_data_from_excel("data/input")
-print(lista_de_data_frame)  
+if __name__ == "__main__":
+    data_frame_list = extract_data_from_excel(path="data/input")
+    combined_data = concat_dataframe(data_frame_list)
+    load_excel(data_frame=combined_data, output_path="data/output", file_name="combined_data")
+    
+    print("Data processing complete. The combined data has been saved to 'data/output/combined_data.xlsx'.")
+    
